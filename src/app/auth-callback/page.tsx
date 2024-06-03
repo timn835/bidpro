171be6048186e366bac6832f03b92378b9545391
Timour Nesterov <timn835@Timours-MacBook-Air.local>
2024-06-03 11:40:23 -0400

    fix suspense bug

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -3,37 +3,40 @@
 import { useRouter, useSearchParams } from "next/navigation";
 import { Loader2 } from "lucide-react";
 import { trpc } from "../_trpc/client";
+import { Suspense } from "react";
 
 const Page = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const origin = searchParams.get("origin");
+	const router = useRouter();
+	const searchParams = useSearchParams();
+	const origin = searchParams.get("origin");
 
-  // this query runs on page load
-  trpc.authCallback.useQuery(undefined, {
-    onSuccess: ({ success }) => {
-      if (success) {
-        // user is synced to database
-        router.push(origin ? `/${origin}` : "dashboard");
-      }
-    },
-    onError: (err) => {
-      if (err.data?.code === "UNAUTHORIZED") {
-        router.push("/api/auth/login");
-      }
-    },
-    retry: true,
-    retryDelay: 2000,
-  });
-  return (
-    <div className="w-full mt-24 flex justify-center">
-      <div className="flex flex-col items-center gap-2">
-        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
-        <h3 className="font-semibold text-xl">Setting up your account ...</h3>
-        <p>You will be redirected automatically.</p>
-      </div>
-    </div>
-  );
+	// this query runs on page load
+	trpc.authCallback.useQuery(undefined, {
+		onSuccess: ({ success }) => {
+			if (success) {
+				// user is synced to database
+				router.push(origin ? `/${origin}` : "dashboard");
+			}
+		},
+		onError: (err) => {
+			if (err.data?.code === "UNAUTHORIZED") {
+				router.push("/api/auth/login");
+			}
+		},
+		retry: true,
+		retryDelay: 2000,
+	});
+	return (
+		<Suspense>
+			<div className="w-full mt-24 flex justify-center">
+				<div className="flex flex-col items-center gap-2">
+					<Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+					<h3 className="font-semibold text-xl">Setting up your account ...</h3>
+					<p>You will be redirected automatically.</p>
+				</div>
+			</div>
+		</Suspense>
+	);
 };
 
 export default Page;
